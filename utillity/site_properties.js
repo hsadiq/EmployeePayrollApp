e581a166d6e881805ffed90ae0fe9544864eb370
hsadiq <ksadik320@gmail.com>
2023-06-14 20:11:49 +0530

    Added Test Server Configurations

--- utillity/site_properties.js
+++ utillity/site_properties.js
@@ -1,5 +1,6 @@
 const site_properties = {
     use_local_storage: "true",
     add_employee_page: "../pages/Index.html",
-    home_page: "../pages/home.html"
+    home_page: "../pages/home.html",
+    server_url: "http://localhost:3000/EmployeePayrollDB/"
 };
